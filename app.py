9677103b77aeffbaab46abb48cae500b2c27a425
Gautham728 <111583612+Gautham728@users.noreply.github.com>
2022-11-06 16:37:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# @Email:  contact@pythonandvba.com
-# @Website:  https://pythonandvba.com
-# @YouTube:  https://youtube.com/c/CodingIsFun
-# @Project:  Sales Dashboard w/ Streamlit
 
 
 
@@ -9,7 +5,7 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ EXCEL ----
@@ -71,7 +67,7 @@ with middle_column:
     st.subheader(" Rating:")
     st.subheader(f"{average_rating} {star_rating}")
 with right_column:
-    st.subheader("Average Sales Per Transaction:")
+    st.subheader("Avg Sales Per Transaction:")
     st.subheader(f" $ {average_sale_by_transaction}")
 
 st.markdown("""---""")
