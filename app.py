546acd5fb2098508d7fa90eca9a43d5eb2354843
Gautham728 <111583612+Gautham728@users.noreply.github.com>
2022-11-10 08:39:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 from PIL import Image
 
 
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="SuperMarket Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ EXCEL ----
 @st.cache
